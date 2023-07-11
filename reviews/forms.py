9b632579ca461461afd2e7895270b90cec701a18
gokul-github-collab <gokulakrishnan0604@gmail.com>
2023-07-11 21:44:36 +0530

    A

---
+++ reviews/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+
+class ReviewForm(forms.Form):
+    user_name = forms.CharField(label="Your Name", max_length=100, error_messages={
+                                "required": "Your name must not be empty"})
--- reviews/urls.py
+++ reviews/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.review),
+    path("thank-you", views.thank_you, name="thank_you"),
 ]

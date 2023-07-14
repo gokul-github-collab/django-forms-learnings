f9aff4600ea9c50822029667377d28d45928df3d
gokul-github-collab <gokulakrishnan0604@gmail.com>
2023-07-14 22:50:36 +0530

    File Uploads Fundementals

--- feedback/settings.py
+++ feedback/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "profiles.apps.ProfilesConfig",
     "reviews.apps.ReviewsConfig",
     'django.contrib.admin',
     'django.contrib.auth',
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'feedback.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR/ "templates"],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

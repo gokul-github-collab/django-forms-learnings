42de4a432d05dc2eb3e152651f2f9bf6c58bc7ff
gokul-github-collab <gokulakrishnan0604@gmail.com>
2023-07-16 22:54:47 +0530

    IDK what I did

--- profiles/views.py
+++ profiles/views.py
@@ -5,6 +5,12 @@ from django.http import HttpResponseRedirect
 # Create your views here.
 
 
+def store_file(file):
+    with open("temp/image.jpg", "wb+") as dest:
+        for chunk in file.chunks():
+            dest.write(chunk)
+
+
 class CreateProfileView(View):
     def get(self, request):
         return render(request, "profiles/create_profile.html")
